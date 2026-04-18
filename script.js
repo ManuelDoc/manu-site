3214c95739c5126e3c133b4f3b5f8fe22faec392
Manuel Doc <manuel@illustrate.digital>
2026-04-19 00:17:47 +0100

    form

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log("Site loaded");
+window.formspree =
+  window.formspree ||
+  function () {
+    (window.formspree.q = window.formspree.q || []).push(arguments);
+  };
+
+window.formspree("initForm", {
+  formElement: "#contact-form",
+  formId: "xzdygglk",
+  data: {
+    source: "website-contact-form",
+  },
+  renderSuccess: () => {
+    window.location.assign("thank-you.html");
+  },
+});
